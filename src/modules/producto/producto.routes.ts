37bc33c88f2dbb8f735332767778f8ccd36b15d1
LucaDelpra <delpratoluca2018@gmail.com>
2025-07-26 19:26:50 -0300

    Implementacion de Zod para el modulo de producto

--- src/modules/producto/producto.routes.ts
+++ src/modules/producto/producto.routes.ts
@@ -6,11 +6,12 @@ import {
   add,
   update,
   remove,
-} from './producto.controller.js'; // Importa desde producto.controller
+} from './producto.controller.js';
 import { adminMiddleware } from 'modules/auth/auth.middleware.js';
 
 export const productoRouter = Router();
 
+// Rutas protegidas y con sanitización
 productoRouter.get('/', findAll);
 productoRouter.get('/:id', findOne);
 productoRouter.post('/', adminMiddleware, sanitizarInputProducto, add);
